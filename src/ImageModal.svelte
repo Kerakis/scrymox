c81292028e78a8b2ed2033197011389b9f884747
Rob Upchurch <rlupchurch3@gmail.com>
2024-04-14 12:27:05 -0400

    Fix body overflow issue in ImageModal.svelte

--- src/ImageModal.svelte
+++ src/ImageModal.svelte
@@ -3,13 +3,21 @@
   export let show = false;
   export let selectedImage = '';
   const dispatch = createEventDispatcher();
+
+  $: {
+    if (show) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
 </script>
 
 {#if show}
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <!-- svelte-ignore a11y-click-events-have-key-events -->
   <div
-    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+    class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
     on:click={() => dispatch('close')}
   >
     <!-- svelte-ignore a11y-img-redundant-alt -->
