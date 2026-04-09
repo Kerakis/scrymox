18e30254779b29955f5472d69b41af08814e7167
Rob Upchurch <rlupchurch3@gmail.com>
2026-04-09 16:17:33 -0400

    Added TailwindCSS Prettier Plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,29 +3,29 @@ import { fileURLToPath } from 'node:url';
 import svelteEslintParser from 'svelte-eslint-parser';
 
 const compat = new FlatCompat({
-  baseDirectory: fileURLToPath(new URL('.', import.meta.url)),
-  recommendedConfig: {
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {},
-  },
+	baseDirectory: fileURLToPath(new URL('.', import.meta.url)),
+	recommendedConfig: {
+		parserOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module'
+		},
+		rules: {}
+	}
 });
 
 export default [
-  {
-    ignores: ['node_modules/**', 'dist/**', 'build/**'],
-  },
-  ...compat.extends('eslint:recommended'),
-  {
-    files: ['**/*.svelte'],
-    languageOptions: {
-      parser: svelteEslintParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-    },
-  },
+	{
+		ignores: ['node_modules/**', 'dist/**', 'build/**']
+	},
+	...compat.extends('eslint:recommended'),
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteEslintParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module'
+			}
+		}
+	}
 ];
